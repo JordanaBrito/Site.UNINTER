2e364f773c28f3e3877887161651c6226c49b100
Jordana <jordanadiasbrito@gmail.com>
2025-07-07 22:21:28 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-<button onclick="Clique aqui para me conhecer mais!"></button>
-<scripy>
-    function exibirAlerta() {
-        alert(!Olá, mundo! Bem vindos ao meu primeiro site!Estou orgulhosa de estar mostrando esse trabalho para vocês!!)
-    }
-</scripy>
