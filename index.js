26e9a267da1341bb5cf17ef60c366fcda7b41de1
Jordana <jordanadiasbrito@gmail.com>
2025-07-07 22:05:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+<button onclick="Clique aqui para me conhecer mais!"></button>
+<scripy>
+    function exibirAlerta() {
+        alert(!Olá, mundo! Bem vindos ao meu primeiro site!Estou orgulhosa de estar mostrando esse trabalho para vocês!!)
+    }
+</scripy>
